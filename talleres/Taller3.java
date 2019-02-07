b0a1a2d79a06b65d6614d4fe17542eaa8e00d1ae
agarciam14 <46933127+agarciam14@users.noreply.github.com>
2019-02-06 22:08:57 -0500

    Update Taller3.java

--- talleres/Taller3.java
+++ talleres/Taller3.java
@@ -53,7 +53,7 @@ public class Taller3 {
      * @param str es la cadena a permutar
      */
     public static void permutation(String str) {
-        permutation("", str);
+        permutationAux("", str);
     }
 
     /**
@@ -68,16 +68,15 @@ public class Taller3 {
      * donde los caracteres no se repiten pero el orden tiene un papel importante.
      *
      */
-    private static void permutation(String prefix, String str) {
-        short n=(short)str.length();
+    private static void permutationAux(String prefix, String str) {
+        int n=str.length();
         if(n==0){
             System.out.println(prefix);
         }else{
-            for(int i=0;i<=n;i++){
-
+            for(int i=0;i<n;i++){
+                permutationAux(prefix+str.charAt(i),str.substring(0,i)+str.substring(i+1,n));
             }
         }
     }
-
 
 }
