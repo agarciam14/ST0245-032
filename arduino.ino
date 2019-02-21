f99f586cb578b3495e08cdbfc0af3e8065cac246
Daniel Hincapie <44171418+dan28hs@users.noreply.github.com>
2019-02-21 17:56:16 -0500

    Update arduino.ino

--- arduino.ino
+++ arduino.ino
@@ -1,15 +1,20 @@
+//potenciometro conectado al pin A0
 #define POT 0
 
+//se declara el pin de cada led
 const int LedRojo =  5;
 const int LedAmarillo =  4;
 const int LedAzul =  3;
 const int LedVerde =  2;
 
-unsigned int voltaje = 0;
-
+//variables
+float valor = 0; //variable para almacenar el valor del potenciometro
+float voltaje = 0; //variable para guardar la conversión a voltios 
 
 void setup() {
+ //iniciamos la comunicación serial
  Serial.begin(9600);
+ //configuración de los pines como salida
  pinMode (LedRojo, OUTPUT);
  pinMode (LedAmarillo, OUTPUT);
  pinMode (LedAzul, OUTPUT);
@@ -17,29 +22,31 @@ void setup() {
 }
 
 void loop() {
-  voltaje = analogRead(POT);
-  Serial.print("Voltaje del potenciometro: ");
-  Serial.println(voltaje);
-  delay(500);
+  valor = analogRead(POT); //se lee el valor del potenciometro y se almacena en esta variable
+  voltaje = (valor * 5)/1000; //conversión del valor en voltios
+  Serial.print("Voltaje del potenciometro: "); 
+  Serial.print(voltaje);
+  Serial.println("V");
+  delay(500); //retardo de 500ms
 
-  if(voltaje > 0 && voltaje <= 255)
+  if(valor > 0 && valor <= 255)  //condición que enciende y apaga el led rojo
     digitalWrite(LedRojo, HIGH);
     else 
     digitalWrite(LedRojo, LOW);
     
-  if(voltaje > 255 && voltaje <= 510)
+  if(valor > 255 && valor <= 510) //condición que enciende y apaga el led amarillo
     digitalWrite(LedAmarillo, HIGH);
-  	else 
+    else 
     digitalWrite(LedAmarillo, LOW);
   
-  if(voltaje > 510 && voltaje <= 765)
+  if(valor > 510 && valor <= 765) //condición que enciende y apaga el led azul
     digitalWrite(LedAzul, HIGH);
-  	else 
+    else 
     digitalWrite(LedAzul, LOW);
   
-  if(voltaje > 765 && voltaje <= 1023)
+  if(valor > 765 && valor <= 1023) //condición que enciende y apaga el led verde
     digitalWrite(LedVerde, HIGH);
-  	else 
+    else 
     digitalWrite(LedVerde, LOW);
  
 }
