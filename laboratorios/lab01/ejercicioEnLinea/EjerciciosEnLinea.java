a7df3266c4d4ff858194596b815f23ca0896c921
Daniel Hincapie <44171418+dan28hs@users.noreply.github.com>
2019-02-24 21:12:32 -0500

    Update EjerciciosEnLinea.java

--- laboratorios/lab01/ejercicioEnLinea/EjerciciosEnLinea.java
+++ laboratorios/lab01/ejercicioEnLinea/EjerciciosEnLinea.java
@@ -101,4 +101,20 @@ public class EjercicioEnLinea
       }
       return groupSumClump(start+rep,nums,target)||groupSumClump(start+rep,nums,target-(nums[start]*rep));
     }
+    public boolean groupSum5(int start, int[] nums, int target) {
+   if(start >= nums.length) {
+    if(target == 0)
+      return true;
+    return false;
+   }
+  
+   if(nums[start] % 5 == 0) {
+    if(start < nums.length - 1 && nums[start+1] == 1)
+      return groupSum5(start + 2, nums, target - nums[start]);
+    return groupSum5(start + 1, nums, target - nums[start]);
+   }
+      if(groupSum5(start + 1, nums, target - nums[start]))
+        return true;
+      return groupSum5(start + 1, nums, target);
+   }
 }
