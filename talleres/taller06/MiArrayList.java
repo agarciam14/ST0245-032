8380f3e5500c0fb244d88f7d78fa6edfc2ced03f
Daniel Hincapie <44171418+dan28hs@users.noreply.github.com>
2019-02-26 11:51:44 -0500

    Create MiArrayList.java

---
+++ talleres/taller06/MiArrayList.java
@@ -0,0 +1,107 @@
+public class MiArrayList {
+    private int size;
+    private static final int DEFAULT_CAPACITY =10;
+    private int elements[]; 
+    
+    /**
+    * El metodo constructor se utiliza para incializar
+    * variables a valores neutros como 0 o null.
+    * El contructor no lleva parámetros en este caso.
+    */
+    public MiArrayList() {
+        size = 0;
+        elements = new int[DEFAULT_CAPACITY];
+    }     
+
+    
+    /**
+    * Tiene la intención de retornar la longitud del objeto
+    * @return longitud del objeto
+    *
+    * El size esta influenciado por las funciones add y del
+    */
+    public int size() {
+        return size;
+    }   
+    
+    /** 
+    * @param e el elemento a guardar
+    * Agrega un elemento e a la última posición de la lista
+    *
+    */
+    public void add(int e) {
+        if(size==elements.length){
+          int[] a= new int[elements.length+10];
+          for(int i=0; i<elements.length;i++){
+            a[i]=elements[i];
+          }
+          a[size]=e;
+          elements=a;
+          size++;
+        }else{
+          elements[size]=e;
+          size++;
+        }
+    }    
+    
+    
+    /** 
+    * @param i es un íncide donde se encuentra el elemento posicionado
+    * Retorna el elemento que se encuentra en la posición i de la lista.
+    *
+    */
+    public int get(int i) throws ArrayIndexOutOfBoundsException {
+        if (i < 0 || i >= size)
+           throw new ArrayIndexOutOfBoundsException("perdon usuario, pero i= " + i + " ,pero size = " + size);
+        else
+           return elements[i];
+    }
+    
+    
+    /**
+    * @param index es la posicion en la cual se va agregar el elemento
+    * @param e el elemento a guardar
+    * Agrega un elemento e en la posición index de la lista
+    *
+    */
+    public void add(int index, int e) throws ArrayIndexOutOfBoundsException {
+        if(index<=size){
+          if(size==elements.length){
+              int[] a= new int[elements.length+10];
+              for(int i=0; i<index;i++){
+                a[i]=elements[i];
+              }
+              a[index]=e;
+              for(int i=index;i<elements.length;i++){
+                a[i+1]=elements[i];
+              }
+              elements=a;
+              size++;
+          }else{
+            for(int i=size; i>index;i--){
+              elements[i]=elements[i-1];
+            }
+            elements[index]= e;
+            size++;
+          }
+        }else{
+          throw new ArrayIndexOutOfBoundsException();
+        }
+    } 
+
+    /**
+    * @param index es la posicion en la cual se va agregar el elemento
+    *
+    * ELimina el elemento  en la posición index de la lista
+    *
+    */
+    public void del(int index)throws ArrayIndexOutOfBoundsException{
+        if(index>=size)
+          throw new ArrayIndexOutOfBoundsException();
+      	for(int i=index;i<size-1;i++){
+          elements[i]=elements[i+1];
+        }
+      	elements[size-1]=0;
+      	size--;
+    }
+}
