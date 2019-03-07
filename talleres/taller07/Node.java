dc39e79adcf3516f227a480f65355aa46ed5cdcc
Daniel Hincapie <dahincapis@eafit.edu.co>
2019-03-06 21:59:19 -0500

    Update Node.java

--- talleres/taller07/Node.java
+++ talleres/taller07/Node.java
@@ -1,9 +1,8 @@
-// Un nodo para una lista simplemente enlazada
 public class Node {
     public int data;
     public Node next;
-    public Node(int data)
-    {
+  
+    public Node(int data){
         next = null;
         this.data = data;
     }
